8f78f8b3416c37eefcc3b59598df7b6f8e74fa44
Piotr Hosa <piotrhosa@gmail.com>
2014-07-01 13:10:32 +0100

    GameMapActivity gets location

--- src/com/pfhosa/sprinklecity/fragments/VirtualMapFragment.java
+++ src/com/pfhosa/sprinklecity/fragments/VirtualMapFragment.java
@@ -1,6 +1,5 @@
 package com.pfhosa.sprinklecity.fragments;
 
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,8 +9,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.pfhosa.sprinklecity.R;
-import com.pfhosa.sprinklecity.location.LocationReceiver;
-import com.pfhosa.sprinklecity.location.LocationTracker;
 
 public class VirtualMapFragment extends Fragment {
 	
@@ -20,13 +17,7 @@ public class VirtualMapFragment extends Fragment {
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
-		getActivity().registerReceiver(new LocationReceiver(), new IntentFilter("locationUpdater"));
-
 		linearLayout = (LinearLayout)inflater.inflate(R.layout.fragment_virtual_map, container, false);
-		
-		LocationTracker locationTracker = new LocationTracker(getActivity());
-		
-		locationTracker.getInitialLocation();
 		
 		distanceTextView = (TextView)getActivity().findViewById(R.id.text_virtual_map_distance);		
 
--- src/com/pfhosa/sprinklecity/ui/GameMapActivity.java
+++ src/com/pfhosa/sprinklecity/ui/GameMapActivity.java
@@ -1,36 +1,161 @@
 package com.pfhosa.sprinklecity.ui;
 
 import android.app.Dialog;
-import android.location.LocationListener;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.pfhosa.sprinklecity.R;
 import com.pfhosa.sprinklecity.fragments.VirtualMapFragment;
 
 public class GameMapActivity extends FragmentActivity implements       
-		GooglePlayServicesClient.ConnectionCallbacks,
-		GooglePlayServicesClient.OnConnectionFailedListener,
-		LocationListener {
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
+
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+	private static final int MILLISECONDS_PER_SECOND = 1000;
+	// Update frequency in seconds
+	public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+	// Update frequency in milliseconds
+	private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+	// The fastest update frequency, in seconds
+	private static final int FASTEST_INTERVAL_IN_SECONDS = 5;
+	// A fast frequency ceiling in milliseconds
+	private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+	LocationRequest mLocationRequest;
+	LocationClient mLocationClient;
+	boolean mUpdatesRequested;
+
+	SharedPreferences mPrefs;
+	Editor mEditor;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_map);
 
-		if(GooglePlayServicesUtil.isGooglePlayServicesAvailable(this))
+		mPrefs = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+		// Get a SharedPreferences editor
+		mEditor = mPrefs.edit();
 
-			if (findViewById(R.id.fragment_container_game_map) != null) {
+		if(servicesConnected()) {
+			mLocationClient = new LocationClient(this, this, this);
+			//mLocationClient.connect();
+			// Start with updates turned off
+			mUpdatesRequested = false;
 
-				if (savedInstanceState != null) {
-					return;
-				}
+			mLocationRequest = LocationRequest.create();
+			// Use high accuracy
+			mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+			// Set the update interval to 5 seconds
+			mLocationRequest.setInterval(UPDATE_INTERVAL);
+			// Set the fastest update interval to 1 second
+			mLocationRequest.setFastestInterval(FASTEST_INTERVAL);			
+			
+		}
+		else {
+			
+		}
 
-				VirtualMapFragment virtualMapFragment = new VirtualMapFragment();
+		if (findViewById(R.id.fragment_container_game_map) != null) {
 
-				getSupportFragmentManager().beginTransaction()
+			if (savedInstanceState != null)
+				return;
+
+
+			VirtualMapFragment virtualMapFragment = new VirtualMapFragment();
+
+			getSupportFragmentManager().beginTransaction()
 				.add(R.id.fragment_container_game_map, virtualMapFragment).commit();
-			}
+		}
 
+	}
+	@Override
+	protected void onPause() {
+		// Save the current setting for updates
+		mEditor.putBoolean("KEY_UPDATES_ON", mUpdatesRequested);
+		mEditor.commit();
+		super.onPause();
+	}
+
+	@Override
+	protected void onStart() {
+		mLocationClient.connect();
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		/*
+		 * Get any previous setting for location updates
+		 * Gets "false" if an error occurs
+		 */
+		if (mPrefs.contains("KEY_UPDATES_ON")) {
+			mUpdatesRequested = mPrefs.getBoolean("KEY_UPDATES_ON", false);
+
+			// Otherwise, turn off location updates
+		} 
+		else {
+			mEditor.putBoolean("KEY_UPDATES_ON", false);
+			mEditor.commit();
+		}
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			/*
+			 * Remove location updates for a listener.
+			 * The current Activity is the listener, so
+			 * the argument is "this".
+			 */
+			mLocationClient.removeLocationUpdates(this);
+		}
+		/*
+		 * After disconnect() is called, the client is
+		 * considered "dead".
+		 */
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	public static class ErrorDialogFragment extends DialogFragment {
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+
+	public void showErrorDialog(int errorCode) {
+		//TODO make new Dialog
 	}
 
 	private boolean servicesConnected() {
@@ -39,14 +164,15 @@ public class GameMapActivity extends FragmentActivity implements
 		// If Google Play services is available
 		if (ConnectionResult.SUCCESS == resultCode) {
 			// In debug mode, log the status
-			Log.d("Location Updates",
-					"Google Play services is available.");
+			Log.d("Location Updates", "Google Play services is available.");
 			// Continue
 			return true;
 			// Google Play services was not available for some reason
-		} else {
+		} 
+		else {
 			// Get the error code
-			int errorCode = connectionResult.getErrorCode();
+			int errorCode = 99;
+			//connectionResult.getErrorCode();
 			// Get the error dialog from Google Play services
 			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
 					errorCode,
@@ -61,10 +187,68 @@ public class GameMapActivity extends FragmentActivity implements
 				// Show the error dialog in the DialogFragment
 				errorFragment.show(getSupportFragmentManager(), "Location Updates");
 			}
+
+			return false;
 		}
 	}
 
-	public static void locationChanged(float distance) {		
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		Log.d("Location changed", "yes");
+
 	}
+	@Override
+	public void onDisconnected() {
+		Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+
+	}
+	@Override
+	public void onLocationChanged(Location location) {
+		String msg = "Updated Location: " +
+				Double.toString(location.getLatitude()) + "," +
+				Double.toString(location.getLongitude());
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,	CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} 
+			catch (IntentSender.SendIntentException e) {
+				e.printStackTrace();
+			}
+		} 
+		else {
+			/*
+			 * If no resolution is available, display a dialog to the
+			 * user with the error.
+			 */
+			showErrorDialog(connectionResult.getErrorCode());
+		}
+	}
+
+
 
 }
